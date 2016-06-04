22f0a9bbeafe6f56052de298179ace992b525b90
hzt <ga203306@yahoo.com.tw>
2016-06-04 22:34:37 +0800

    Add files via upload

---
+++ w.ino
@@ -0,0 +1,158 @@
+#include <Adafruit_CC3000.h>
+#include <ccspi.h>
+#include <SPI.h>
+#include <string.h>
+#include "utility/debug.h"
+
+
+#define ADAFRUIT_CC3000_IRQ   3 
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); 
+
+#define WLAN_SSID       "hzt"           
+#define WLAN_PASS       "ga503306"
+
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define IDLE_TIMEOUT_MS  3000 
+
+
+#define WEBSITE      "210.240.202.114"
+#define WEBPAGE      "/testard.php"
+
+uint32_t ip;
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println(F("Hello, CC3000!\n")); 
+
+  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+  
+
+  Serial.println(F("\nInitializing..."));
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    while(1);
+  }
+  
+  Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); 
+  }  
+
+
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+ip = cc3000.IP2U32(210,240,202,114); //指定
+
+
+  Serial.print(WEBSITE); Serial.print(F(" -> "));
+  while (ip == 0) {
+    if (! cc3000.getHostByName(WEBSITE, &ip)) {
+      Serial.println(F("Couldn't resolve!"));
+    }
+    delay(500);
+  }
+
+  cc3000.printIPdotsRev(ip);
+  
+
+String sendthis = "sendthis= yo test";
+  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
+  if (www.connected()) {
+    www.println("POST /testard.php HTTP/1.1");
+    www.println("Host: 210.240.202.114");
+    www.println("User-Agent: Arduino/1.0");
+    www.println("Connection: close");
+    www.println("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+    www.print("Content-Length: ");
+    www.println(sendthis.length());
+    www.println();
+    www.println(sendthis);
+
+
+  } else {
+    Serial.println(F("Connection failed"));    
+    return;
+  }
+
+  Serial.println(F("-------------------------------------"));
+  
+  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+  /*unsigned long lastRead = millis();
+  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+    while (www.available()) {
+      char c = www.read();
+      Serial.print(c);
+      lastRead = millis();
+    }
+  }
+  */
+  int i =0;
+  String reg;
+ 
+
+  while (www.connected()) {
+      while (www.available()) {
+
+      // Read answer
+      char c = www.read();
+      
+      reg =reg + c;
+      
+      }
+    }
+  www.close();
+  
+   Serial.println(reg); 
+
+  
+  Serial.println(F("-------------------------------------"));
+  
+  Serial.println(F("\n\nDisconnecting"));
+  cc3000.disconnect();
+  
+}
+
+void loop(void)
+{
+ delay(1000);
+}
+
+
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
+}
